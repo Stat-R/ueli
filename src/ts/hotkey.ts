0388d84af6201ddb066e8512040b87e5fea10bbf
khanhas <xuankhanh963@gmail.com>
2018-07-09 01:23:36 +0700

    Hotkey parser

---
+++ src/ts/hotkey.ts
@@ -0,0 +1,32 @@
+import { StringHelpers } from "./helpers/string-helpers";
+
+export class Hotkey {
+    private ctrl: boolean = false;
+    private shift: boolean = false;
+    private alt: boolean = false;
+    private meta: boolean = false;
+    private letter: string = "";
+
+    constructor(raw: string) {
+        const splitted = raw.split("+");
+        splitted.forEach((key) => {
+            key = key.toLowerCase();
+            key = StringHelpers.removeWhiteSpace(key);
+            switch (key) {
+                case "ctrl":    this.ctrl = true;   break;
+                case "shift":   this.shift = true;  break;
+                case "alt":     this.alt = true;    break;
+                case "meta":    this.meta = true;   break;
+                default: this.letter = key;
+            }
+        });
+    }
+
+    public validateWithEvent(event: KeyboardEvent): boolean {
+        return (event.ctrlKey === this.ctrl)
+            && (event.shiftKey === this.shift)
+            && (event.altKey === this.alt)
+            && (event.metaKey === this.meta)
+            && (event.key.toLowerCase() === this.letter);
+    }
+}
