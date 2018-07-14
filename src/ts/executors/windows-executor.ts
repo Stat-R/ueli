c8fae7c1c2e817ff1937f533cb9b74db60735943
khanhas <xuankhanh963@gmail.com>
2018-07-15 02:39:36 +0700

    Create its own Taskbar instead

--- src/ts/executors/windows-executor.ts
+++ src/ts/executors/windows-executor.ts
@@ -1,18 +1,15 @@
 import { Executor } from "./executor";
+import { Taskbar } from "taskbar-node";
 
 export class ProcessExecutor implements Executor {
     public readonly hideAfterExecution = true;
     public readonly resetUserInputAfterExecution = true;
     public readonly logExecution = false;
 
-    private bringToTop: (hwnd: number) => void;
-
-    constructor(bringToTop: (hwnd: number) => void) {
-        this.bringToTop = bringToTop;
-    }
-
     public execute(executionArgument: string): void {
         const hwnd = parseInt(executionArgument.substring(5), 10);
-        this.bringToTop(hwnd);
+        const taskbar = new Taskbar();
+        taskbar.bringAppToTop(hwnd);
+        taskbar.destruct();
     }
 }
