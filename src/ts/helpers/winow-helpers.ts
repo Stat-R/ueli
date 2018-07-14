2412adebcbaa53b027b5754c2eb55e1d25aa7cbb
khanhas <xuankhanh963@gmail.com>
2018-07-15 02:35:45 +0700

    Update visual

--- src/ts/helpers/winow-helpers.ts
+++ src/ts/helpers/winow-helpers.ts
@@ -4,10 +4,10 @@ export class WindowHelpers {
                                         userInputHeight: number,
                                         searchResultHeight: number,
                                         musicPlayerHeight: number): number {
-        return searchResultCount === 0 ? (userInputHeight + musicPlayerHeight) :
+        return 40 + (searchResultCount === 0 ? (userInputHeight + musicPlayerHeight) :
             (searchResultCount >= maxSearchResultCount
                 ? WindowHelpers.calculateMaxWindowHeight(userInputHeight, maxSearchResultCount, searchResultHeight)
-                : userInputHeight + (searchResultCount * searchResultHeight));
+                : userInputHeight + (searchResultCount * searchResultHeight)));
     }
 
     public static calculateMaxWindowHeight(userInputHeight: number, maxSearchResultCount: number, searchResultHeight: number): number {
