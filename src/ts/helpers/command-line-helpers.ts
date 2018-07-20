7620ce2021883dfab9c8344f3ef865e92f48cfe1
khanhas <xuankhanh963@gmail.com>
2018-07-20 23:09:45 +0700

    Autocomplete commands/programs and parameter

--- src/ts/helpers/command-line-helpers.ts
+++ src/ts/helpers/command-line-helpers.ts
@@ -1,25 +1,18 @@
-import { CommandLineProgram } from "../command-line-program";
 import { StringHelpers } from "./string-helpers";
+import { CommandLineProgram } from "../command-line-program";
 
 export class CommandLineHelpers {
     public static readonly commandLinePrefix = ">";
 
     public static buildCommand(executionArgument: string): CommandLineProgram {
         const words = StringHelpers.stringToWords(executionArgument);
-        const command = words[0].replace(CommandLineHelpers.commandLinePrefix, "");
-        const args = [] as string[];
+        words[0] = words[0].replace(CommandLineHelpers.commandLinePrefix, "");
 
-        for (let i = 0; i < words.length; i++) {
-            if (i === 0) {
-                continue;
-            }
-
-            args.push(words[i]);
-        }
+        words.unshift("-Command");
 
         return {
-            args,
-            name: command,
+            args: words,
+            name: "powershell.exe",
         } as CommandLineProgram;
     }
 }
