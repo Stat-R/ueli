b1cbfe47983cef4d5e9bdba441a45850c98fd0be
khanhas <xuankhanh963@gmail.com>
2018-10-09 16:16:25 +0700

    fix: returns directly instead of creating new Promise

--- src/ts/searcher-online/youtube-searcher.ts
+++ src/ts/searcher-online/youtube-searcher.ts
@@ -4,44 +4,42 @@ import * as cheerio from "cheerio";
 import fetch from "node-fetch";
 
 export class YoutubeSearcher implements Searcher {
-    public getSearchResult(userInput: string): Promise<SearchResultItem[]> {
+    public async getSearchResult(userInput: string): Promise<SearchResultItem[]> {
         userInput = userInput.replace("y!", "");
-        return new Promise<SearchResultItem[]>((resolve) => {
-            fetch(`https://youtube.com/results?search_query=${encodeURIComponent(userInput)}`)
-                .then((response) => response.text())
-                .then((response) => {
-                    const $ = cheerio.load(response);
+        return fetch(`https://youtube.com/results?search_query=${encodeURIComponent(userInput)}`)
+            .then((response) => response.text())
+            .then((response) => {
+                const $ = cheerio.load(response);
 
-                    const results = $(".yt-lockup-video")
-                        .filter((_i, e) => !("data-ad-impressions" in e.attribs));
+                const results = $(".yt-lockup-video")
+                    .filter((_i, e) => !("data-ad-impressions" in e.attribs));
 
-                    const items = [] as SearchResultItem[];
+                const items = [] as SearchResultItem[];
 
-                    $(results).each((_i, el) => {
-                        const title = $(el).find("h3.yt-lockup-title a");
-                        const link = title.attr("href");
-                        const channel = $(el).find("div.yt-lockup-byline a");
+                $(results).each((_i, el) => {
+                    const title = $(el).find("h3.yt-lockup-title a");
+                    const link = title.attr("href");
+                    const channel = $(el).find("div.yt-lockup-byline a");
 
-                        const thumbEl = $(el).find("span.yt-thumb-simple img");
-                        let thumbLink = thumbEl.attr("src");
+                    const thumbEl = $(el).find("span.yt-thumb-simple img");
+                    let thumbLink = thumbEl.attr("src");
 
-                        if (thumbLink.match(/\.gif/)) {
-                            thumbLink = thumbEl.attr("data-thumb");
-                        }
+                    if (thumbLink.match(/\.gif/)) {
+                        thumbLink = thumbEl.attr("data-thumb");
+                    }
 
-                        thumbLink = thumbLink.substring(0, thumbLink.indexOf(".jpg") + 4);
+                    thumbLink = thumbLink.substring(0, thumbLink.indexOf(".jpg") + 4);
 
-                        items.push({
-                            breadCrumb: [channel.text()],
-                            executionArgument: `https://youtube.com${link}`,
-                            icon: `<image href="${thumbLink}" x="0" y="0" width="32" height="32"/>`,
-                            name: title.text(),
-                        } as SearchResultItem);
-                    });
+                    items.push({
+                        breadCrumb: [channel.text()],
+                        executionArgument: `https://youtube.com${link}`,
+                        icon: `<image href="${thumbLink}" x="0" y="0" width="32" height="32"/>`,
+                        name: title.text(),
+                    } as SearchResultItem);
+                });
 
-                    resolve(items);
-                })
-                .catch(() => resolve([]));
-        });
+                return items;
+            })
+            .catch(() => [] as SearchResultItem[]);
     }
 }
