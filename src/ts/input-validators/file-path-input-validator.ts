c44449610fbb0d08c679ce9e661a7ccef9207377
khanhas <xuankhanh963@gmail.com>
2018-11-01 13:43:35 +0700

    fix: check case where user input is a full valid path in cwd mode

--- src/ts/input-validators/file-path-input-validator.ts
+++ src/ts/input-validators/file-path-input-validator.ts
@@ -3,12 +3,16 @@ import { InputValidator } from "./input-validator";
 import { platform } from "os";
 
 export class FilePathInputValidator implements InputValidator {
+    private regex = Injector.getFilePathRegExp(platform());
+
     public isValidForSearchResults(userInput: string, cwd: string | undefined): boolean {
-        if (cwd) {
+        const isValidPath = this.regex.test(userInput);
+
+        if (!isValidPath && cwd) {
             userInput = `${cwd}${userInput}`;
+            return this.regex.test(userInput);
         }
 
-        const regex = Injector.getFilePathRegExp(platform());
-        return regex.test(userInput);
+        return isValidPath;
     }
 }
